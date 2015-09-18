93e2951dc98de07baa939e9dd2f144b103207553
Andre Garrett <andregarrett@gmail.com>
2015-09-18 11:42:11 -0700

    self-study notes

---
+++ 202/folland/auto/folland.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "folland"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("siunitx" "version-1-compatibility")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "mathptmx"
+    "mathpazo"
+    "mathrsfs"
+    "amsfonts"
+    "amsmath"
+    "amssymb"
+    "siunitx"
+    "fixltx2e"
+    "multirow"
+    "dsfont")
+   (TeX-add-symbols
+    "inv"
+    "n")))
+
